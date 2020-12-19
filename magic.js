a19fb98bba9d6e0d1a12f5296d64e28198790d07
Steellow <hankilapio@gmail.com>
2020-12-19 19:02:23 +0200

    Create printNames()

--- magic.js
+++ magic.js
@@ -3,5 +3,14 @@ var names;
 $(document).ready(function() {
     $.getJSON('names.json').then(function(data) {
         names = data.names
+        printNames()
     })
 });
+
+function printNames() {
+    $("tbody").html("")
+
+    names.forEach(person => {
+        $("tbody").after('<tr><td>' + person.name + '</td><td>' + person.amount + '</td></tr>')
+    });
+}
