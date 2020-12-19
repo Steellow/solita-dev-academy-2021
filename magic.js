0ee595b7b0ab246bde26812d3a461f7a6a6c739c
Steellow <hankilapio@gmail.com>
2020-12-19 19:38:20 +0200

    Create sortReverseAlphavetical()

--- magic.js
+++ magic.js
@@ -31,10 +31,18 @@ $("#name").click(function() {
     printNames()
 })
 
-function sortAlphabetical(a, b) {
+function sortAlphabetical() {
     names.sort(function(a, b) {
         if (a.name < b.name) return -1
         if (a.name > b.name) return 1
+        return 0
+    })
+}
+
+function sortReverseAlphabetical() {
+    names.sort(function(a, b) {
+        if (a.name < b.name) return 1
+        if (a.name > b.name) return -1
         return 0
     })
 }
