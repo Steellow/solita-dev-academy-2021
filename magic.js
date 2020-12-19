98fa77c6ac947c06fb766f7e6cf259af1da1063f
Steellow <hankilapio@gmail.com>
2020-12-19 19:26:55 +0200

    Create calculateTotal()

--- magic.js
+++ magic.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $.getJSON('names.json').then(function(data) {
         names = data.names
         printNames()
+        calculateTotal()
     })
 });
 
@@ -13,4 +14,13 @@ function printNames() {
     names.forEach(person => {
         $("tbody").after('<tr><td>' + person.name + '</td><td>' + person.amount + '</td></tr>')
     });
+}
+
+function calculateTotal() {
+    let total = 0
+    names.forEach(person => {
+        total += person.amount
+    });
+
+    $("#total").html(total)
 }
